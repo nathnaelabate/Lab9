208ede014240ccf9e4cbf228028e832fd28770ef
nathnaelabate <nathnaelabate56@gmail.com>
2024-04-11 12:53:54 -0400

    Encode working

--- lab9.py
+++ lab9.py
@@ -1,7 +1,9 @@
-def encoder(num):
+def encode_number(num):
+    num_str = str(num)
+    encoded_num_str = ''
+    for digit in num_str:
+        encoded_digit = str(int(digit) + 3)
+        encoded_num_str += encoded_digit
+    encoded_num = int(encoded_num_str)
+    return encoded_num
 
-    for i in range(len(num)):
-        return i
-
-value = (input("Enter a number: "))
-print(encoder(value))
